85cf5558ad5475d687feef02a700c37ff8fe9a98
XxprogunxX <javierhdz754@gmail.com>
2025-02-25 11:49:35 -0600

    backend en la nube con los datos

--- src/db.ts
+++ src/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async (): Promise<void> => {
   try {
-    const uri = 'mongodb://atlas-sql-67b7e145f1da1643d2e04f73-uwlht.a.query.mongodb.net/eggsync?ssl=true&authSource=admin';
+    const uri = 'mongodb+srv://javierhdz754:AyrmLSUp4ZsEvb6W@cluster0.uwlht.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
     await mongoose.connect(uri);
     console.log(">>>>> Base de datos conectada (: <<<<<");
   } catch (error) {
